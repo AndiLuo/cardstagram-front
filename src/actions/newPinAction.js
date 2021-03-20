da72d40affdb207ee2c2b9a94042b57e03a4e268
unknown <aluo5731@gmail.com>
2021-03-19 19:01:59 -0700

    Update newPinAction.js

--- src/actions/newPinAction.js
+++ src/actions/newPinAction.js
@@ -2,7 +2,7 @@
 //Takes in data arg, dispatches data to EXPRESS server and POSTS the data into the db
 export function createPin(data){
     return dispatch => {
-        return fetch('/api/pins', { 
+        return fetch('https://cardstagram.herokuapp.com/api/pins', { 
             method: 'POST', 
              headers: {
                 'Accept': 'application/json',
