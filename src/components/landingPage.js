3f3582c605192ed8769ec0c92850211f27130cca
unknown <aluo5731@gmail.com>
2021-04-27 15:17:21 -0700

    added form validation for create card page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -78,7 +78,6 @@ function LandingPage(props) {
                 </Button>
             </span>
             {loading ?  <Spinner/> : <PinContainer/> }
-            {loading ?  <Spinner/> : <Blank/>}
             </div>
         </div>
     )
